77d9a2f7a76746aea717c9afd524dd8168e32182
Monique Robinson <monique.robinson2@gmail.com>
2018-06-28 15:54:41 -0400

    changed login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Search from "./pages/Search";
-import LoginForm from "./pages/Collection";
+import LoginForm from "./pages/Home";
 import AddForm from "./pages/Add";
 
 
@@ -12,10 +12,9 @@ const App = () => {
       <div className="container-fluid">
         <Navbar />
         <Switch>
-        {/*
-        <Route exact path="/" component={Home} />
-        */}
-        <Route exact path="/collection" component={LoginForm} />
+        
+        <Route exact path="/" component={LoginForm} />
+        {/*<Route exact path="/collection" component={} />*/}
         <Route exact path="/search" component={Search} />
         <Route exact path="/add" component={AddForm} />
           <Route render={() => <h1 className="text-center">You didn't match a route!</h1>}
