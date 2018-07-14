f947f0b07dbfcfe1988bf730ac61fce05db73ef1
Monique Robinson <monique.robinson2@gmail.com>
2018-07-13 22:18:41 -0400

    changed page names

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = props => {
     return (
       <Menu secondary>
         <Menu.Item><Image className="navbar-brand" style={imageStyle} src={title} /></Menu.Item>
-        <Menu.Item name='collection'><NavLink className="nav-link" color="black" to="/collection" activeClassName="active">Collection</NavLink></Menu.Item>
+        <Menu.Item name='collection'><NavLink className="nav-link" color="black" to="/collection" activeClassName="active"> My Collection</NavLink></Menu.Item>
         <Menu.Item name='Add'><NavLink className="nav-link" to="/add" activeClassName="active">Add</NavLink></Menu.Item>
         <Menu.Item name='Search'><NavLink className="nav-link" to="/search" activeClassName="active">Search</NavLink></Menu.Item>
         <Menu.Menu position='right'>
@@ -56,23 +56,3 @@ const Navbar = props => {
 
 export default Navbar;
 
-// <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
-//         <Image className="navbar-brand" style={imageStyle} src={title} />
-//         <div className="collapse navbar-collapse" id="navbarNav">
-//           <ul className="navbar-nav">
-//             <li className="nav-item">
-//               <NavLink className="nav-link" to="/collection" activeClassName="active">Collection</NavLink>
-//             </li>
-//             <li className="nav-item">
-//               <NavLink className="nav-link" to="/add" activeClassName="active">Add</NavLink>
-//             </li>
-//             <li className="nav-item">
-//               <NavLink className="nav-link" to="/search" activeClassName="active">Search</NavLink>
-//             </li>
-//           </ul>
-//           <Button basic color='black' circular="true" position="right" onClick={this.logout} >
-//             Logout
-//       </Button>
-//         </div>
-//       </nav>
